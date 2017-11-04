c02675930148be650ac9d2d902e8cc4dc9317d33
ngot <zhuanghengfei@gmail.com>
2017-11-04 20:35:42 +0800

    save

---
+++ build.cmd
@@ -0,0 +1,95 @@
+@echo off
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+if defined ProgramFiles(x86) (set HOST_ARCH=amd64) else (set HOST_ARCH=i386)
+set TARGET_ARCH=!HOST_ARCH!
+
+set BUILD_TYPE=release
+set XP=""
+set MT=/m
+set ARG_ERROR=no
+
+for %%a in (%*) do (
+    set ARG_ERROR=yes
+
+    if "%%a"=="amd64" (
+    	set TARGET_ARCH=amd64
+        set ARG_ERROR=no
+    )
+
+    if "%%a"=="i386" (
+    	set TARGET_ARCH=i386
+        set ARG_ERROR=no
+    )
+
+    if "%%a"=="release" (
+    	set BUILD_TYPE=release
+        set ARG_ERROR=no
+    )
+
+    if "%%a"=="debug" (
+    	set BUILD_TYPE=debug
+        set ARG_ERROR=no
+        set MT=
+    )
+
+    if "%%a"=="xp" (
+    	set XP="_XP"
+        set ARG_ERROR=no
+    )
+
+    if "%%a"=="clean" (
+    	set BUILD_TYPE=clean
+        set ARG_ERROR=no
+    )
+
+    if "%%a"=="-h" goto usage
+    if "%%a"=="--help" goto usage
+
+    if "!ARG_ERROR!"=="yes" (
+        echo illegal option "%%a"
+        goto usage
+    )
+)
+
+if "!BUILD_TYPE!"=="clean" (
+    if "!TARGET_ARCH!"=="amd64" (
+        rmdir /S/Q out\Windows_amd64_Debug
+        rmdir /S/Q out\Windows_amd64_Release
+        rmdir /S/Q bin\Windows_amd64_Debug
+        rmdir /S/Q bin\Windows_amd64_Release
+    )
+
+    if "!TARGET_ARCH!"=="i386" (
+        rmdir /S/Q out\Windows_i386_Debug
+        rmdir /S/Q out\Windows_i386_Release
+        rmdir /S/Q bin\Windows_i386_Debug
+        rmdir /S/Q bin\Windows_i386_Release
+    )
+    goto out
+)
+
+if "!TARGET_ARCH!"=="amd64" (set Platform=x64) else (set Platform=Win32)
+
+msbuild fibjs_vender.sln /t:Build /p:Configuration=!BUILD_TYPE!;Platform=!Platform!;PlatformToolset=v141!XP! !MT!
+
+goto out
+
+:usage
+echo.
+echo Usage: build [release ^| debug ^| i386 ^| amd64 ^| noxp ^| clean] [-h]
+echo Options:
+echo   release, debug: 
+echo       Specifies the build type.
+echo   i386, amd64:
+echo       Specifies the architecture for code generation.
+echo   xp:
+echo       Build as xp support.
+echo   clean: 
+echo       Clean the build folder.
+echo   -h, --help:
+echo       Print this message and exit.
+echo.
+
+:out
---
+++ tools/ci.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env fibjs
+
+const copy = require('@fibjs/copy');
+const mkdirp = require('@fibjs/mkdirp');
+const fs = require('fs');
+const path = require('path');
+
+const venderPath = path.join(__dirname, '../vender');
+const prjRootPath = path.join(__dirname, '..');
+
+const ignores = [
+  'out',
+  'bin',
+  'vender',
+  '.git',
+  'build.cmd',
+  'fibjs_vender.sln',
+].map(p => path.join(prjRootPath, p));
+
+if (!fs.exists(venderPath)) {
+  mkdirp(venderPath);
+}
+
+copy(prjRootPath, venderPath, (data, dir) => {
+  const ignore = ignores.some(p => dir.src.startsWith(p));
+  if (ignore) {
+    return false
+  }
+
+  return data;
+});
+
+if (process.platform !== 'win32') {
+  const execCmd = path.join(venderPath, 'build');
+  fs.chmod(execCmd, 0700);
+  process.chdir(venderPath);
+  process.run(execCmd, ['-j']);
+} else {
+  process.chdir(prjRootPath);
+  const execCmd = path.join(prjRootPath, 'build');
+  process.run(execCmd, []);
+}
